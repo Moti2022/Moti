61eb3c719e64b9cf97af0e97aec758278bbc2195
Nicklas Stellberg <nicklas.stellberg@hotmail.com>
2022-10-27 11:12:30 +0300

    First commit

---
+++ src/test/java/hh/SWD4TN022/Moti/MotiApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.SWD4TN022.Moti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
