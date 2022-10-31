6bbfb2e2857b5ed7fc312105222ffc3ce103ddb7
Nicklas Stellberg <nicklas.stellberg@hotmail.com>
2022-10-31 09:56:38 +0200

    changed query.description

--- src/main/java/hh/SWD4TN022/Moti/web/QueryController.java
+++ src/main/java/hh/SWD4TN022/Moti/web/QueryController.java
@@ -1,23 +1,20 @@
 package hh.SWD4TN022.Moti.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import hh.SWD4TN022.Moti.domain.Query;
+
 import hh.SWD4TN022.Moti.domain.QueryRepository;
-import hh.SWD4TN022.Moti.domain.Question;
-import hh.SWD4TN022.Moti.domain.QuestionRepository;
+
 
 @Controller
 public class QueryController {
 	
 	
 	@Autowired
-	private QuestionRepository queryRepo;
+	private QueryRepository queryRepo;
 
 	@GetMapping("/querylist")
 	public String getQuery(Model model) {
