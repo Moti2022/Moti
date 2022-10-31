dc4b7387139bc4c3f17b7f2f40756e2e227f1e46
Iiris <iiris-harju@hotmail.com>
2022-10-31 10:22:44 +0200

    Rest questions

--- src/main/java/hh/SWD4TN022/Moti/web/QuestionController.java
+++ src/main/java/hh/SWD4TN022/Moti/web/QuestionController.java
@@ -1,10 +1,14 @@
 package hh.SWD4TN022.Moti.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import hh.SWD4TN022.Moti.domain.Question;
 import hh.SWD4TN022.Moti.domain.QuestionRepository;
 
 @Controller
@@ -17,5 +21,11 @@ public class QuestionController {
 	public String listQuestions(Model model) {
 		model.addAttribute("questions", questionRepo.findAll());
 		return "questionlist";
+	}
+
+	// Rest all questions
+	@GetMapping("/questions")
+	public @ResponseBody List<Question> questionListRest() {
+		return (List<Question>) questionRepo.findAll();
 	}
 }
