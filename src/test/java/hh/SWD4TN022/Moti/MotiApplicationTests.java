c51347a887e5ff8b8b283d6f68be0d049a6356ff
Nicklas Stellberg <nicklas.stellberg@hotmail.com>
2022-11-03 12:05:36 +0200

    added smoke test for controllers

--- src/test/java/hh/SWD4TN022/Moti/MotiApplicationTests.java
+++ src/test/java/hh/SWD4TN022/Moti/MotiApplicationTests.java
@@ -1,13 +1,32 @@
 package hh.SWD4TN022.Moti;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import hh.SWD4TN022.Moti.web.QueryController;
+import hh.SWD4TN022.Moti.web.QuestionController;
+
 
 @SpringBootTest
 class MotiApplicationTests {
 
+	@Autowired
+	private QueryController queryController;
+	
+	@Autowired
+	private QuestionController questionController;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(queryController).isNotNull();
+	}
+	
+	@Test
+	public void contextLoads2() {
+		assertThat(questionController).isNotNull();
 	}
 
 }
